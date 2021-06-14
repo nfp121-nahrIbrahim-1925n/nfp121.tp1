632c922f923a7c01ddf709e9452f74b6f22dde23
nadinehawat99 <67381733+nadinehawat99@users.noreply.github.com>
2021-06-14 19:57:32 +0300

    Update AuditeurCNAM.java

--- tp1/question3/AuditeurCNAM.java
+++ tp1/question3/AuditeurCNAM.java
@@ -1,87 +1,57 @@
 package question3;
 
-/**
- * NFP121 TpIntroduction, usage de BlueJ et du "Submitter".
- * 
- * @version septembre 2009
- * @author à compléter
- * @see java.lang.String, java.lang.Math
- */
+
 public class AuditeurCNAM {
-    /** l'attribut nom de chaque auditeur. */
+  
     private String nom;
-    /** l'attribut prenom de chaque auditeur. */
+   
     private String prenom;
-    /** l'attribut matricule de chaque auditeur. */
+  
     private String matricule;
 
-    /**
-     * "Création", le constructeur d'un auditeur avec son nom, son prénom et son
-     * matricule.
-     * 
-     * @param nom
-     *            le nom de l'auditeur
-     * @param prenom
-     *            son prénom
-     * @param matricule
-     *            sur la carte d'inscription, près de la photo
-     */
+  
     public AuditeurCNAM(String nom, String prenom, String matricule) {
         this.nom = nom;
         this.prenom = prenom;
         this.matricule = matricule;
     }
 
-    /**
-     * le login au Cnam : 6 premières lettres du nom suivies de la première
-     * lettre du prénom séparées de '_' . le login retourné est en minuscules,
-     * le trait d'union, ou spéciaux <i>(pour unix)</i> sont remplacés par des
-     * '_' pas de caractères accentués pour le login voir les classes
-     * prédéfines, java.lang.String : les méthodes replaceAll, toLowerCase et
-     * substring java.lang.Math : la méthode min<br>
-     * <b>BlueJ : Menu Aide</b>
-     * 
-     * @return le login du Cnam simplifié, sans les adaptations dues aux
-     *         homonymes...
-     */
+    
     public String login() {
-        return "";// à compléter
+        if (this.nom !=null && this.prenom !=null){
+            String login = "";
+            
+            login += nom().length() > 6 ? nom().substring(0,6) : nom();
+            login += "_";
+            login += prenom().length() > 1 ? prenom().substring(0,1) : prenom();
+            
+            login = login.toLowerCase();
+            login = login.replaceAll("Ã©", "e");
+            login = login.replaceAll("Ã¨", "e");
+            login = login.replaceAll("Ãª", "e");
+            login = login.replaceAll("Ã ", "a");
+            login = login.replaceAll("Ã¢", "a");
+           
+            login = login.replaceAll("[^a-z0-9]", "_");
+            return login;
+        }
+        return "Attention ! Le nom et le prÃ©nom ne peuvent pas Ãªtre null";
     }
 
-    /**
-     * Lecture du nom de l'auditeur.
-     * 
-     * @return son nom
-     */
+   
     public String nom() {
-        return null;// à compléter
+        return this.nom;
     }
 
-    /**
-     * Lecture du prénom de l'auditeur.
-     * 
-     * @return son prénom
-     */
+  
     public String prenom() {
-        return null;// à compléter
+        return this.prenom;
     }
 
-    /**
-     * Lecture du matricule de l'auditeur.
-     * 
-     * @return son matricule
-     */
     public String matricule() {
-        return null;// à compléter
+        return this.matricule;
     }
 
-    /**
-     * méthode toString ( méthode redéfinie de la classe Object).
-     * 
-     * @return la concaténation du nom, du prénom et du login, selon cette
-     *         syntaxe
-     *         <code>nom() + " " + prenom() +  " login : " + login()</code>
-     */
     @Override
     public String toString() {
         return nom() + " " + prenom() + " login : " + login();
