7ffbf3636c14fe27b1e328cfcfcf85c95625df0f
nadinehawat99 <67381733+nadinehawat99@users.noreply.github.com>
2021-06-14 19:55:08 +0300

    Update Picture.java

--- tp1/question2/Picture.java
+++ tp1/question2/Picture.java
@@ -4,33 +4,16 @@ import question1.Circle;
 import question1.Square;
 import question1.Triangle;
 
-/**
- * This class represents a simple picture. You can draw the picture using the
- * draw method. But wait, there's more: being an electronic picture, it can be
- * changed. You can set it to black-and-white display and back to colors (only
- * after it's been drawn, of course).
- * 
- * This class was written as an early example for teaching Java with BlueJ.
- * 
- * @author Michael Kolling and David J. Barnes
- * @version 1.1 (24 May 2001)
- */
 public class Picture {
     private Square wall;
     private Square window;
     private Triangle roof;
-    private Circle sun;
+    private Circle sun, sun2;
+    private boolean TerreFixe;
 
-    /**
-     * Constructor for objects of class Picture
-     */
-    public Picture() {
-        // nothing to do... instance variables are automatically set to null
-    }
-
-    /**
-     * Draw this picture.
-     */
+    
+   
+ 
     public void draw() {
         wall = new Square();
         wall.moveVertical(80);
@@ -50,37 +33,57 @@ public class Picture {
         roof.makeVisible();
 
         sun = new Circle();
-        sun.changeColor("yellow");
+        sun.changeColor("blue");
         sun.moveHorizontal(180);
         sun.moveVertical(-10);
         sun.changeSize(60);
         sun.makeVisible();
+        
+        sun2 = new Circle();
+        sun2.changeColor("yellow");
+        sun2.moveHorizontal(-15);
+        sun2.moveVertical(-10);
+        sun2.changeSize(60);
+        sun2.makeVisible();
     }
 
-    /**
-     * Change this picture to black/white display
-     */
+   
     public void setBlackAndWhite() {
-        if (wall != null) // only if it's painted already...
+        if (wall != null) 
         {
             wall.changeColor("black");
             window.changeColor("white");
             roof.changeColor("black");
             sun.changeColor("black");
+            sun2.changeColor("black");
         }
     }
 
-    /**
-     * Change this picture to use color display
-     */
     public void setColor() {
-        if (wall != null) // only if it's painted already...
+        if (wall != null) 
         {
             wall.changeColor("red");
             window.changeColor("black");
             roof.changeColor("green");
-            sun.changeColor("yellow");
+            sun.changeColor("blue");
+            sun2.changeColor("yellow");
         }
     }
+   
+    public void seCoucher(){
+        if(!TerreFixe){
+            TerreFixe = true;
+            sun.slowMoveVertical(250);
+        }
+    }
+    
+  
+    public void seLever(){
+        if(TerreFixe){
+            TerreFixe = false;
+            sun.slowMoveVertical(-250);
+        }
+    }
+
 
 }
